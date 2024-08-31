f08945886aa3b6b81b57b508a7cda0507925bcdc
Prem Modha <premmodhaofficial@gmail.com>
2024-08-31 12:07:49 +0530

    conflict resolved

--- skbackend/api/utils.py
+++ skbackend/api/utils.py
@@ -1,20 +1,26 @@
-from api.models import Student
-from django.core.mail import send_mail
-from django.conf import settings
 import random
 
+from api.models import Student
+from django.conf import settings
+from django.core.mail import send_mail
 
-def send_email_to_client(request,pk):
-  subject = "OTP for Verification Email id"
-  OTP = random. randint(1000, 9999)
 
-  if request.method == "PUT":
-    students = Student.objects.get(pk=pk)
-    name = students.name
-    email = students.email
-  
-  message = f"Dear {name}, OTP for email verification is {OTP}"
-  from_email = settings.EMAIL_HOST_USER
-  recipient = [email]
-  
-  send_mail(subject=subject,message=message,from_email=from_email,recipient_list=recipient)
+def send_email_to_client(request, pk):
+    subject = "OTP for Verification Email id"
+    OTP = random.randint(1000, 9999)
+
+    if request.method == "PUT":
+        students = Student.objects.get(pk=pk)
+        name = students.name
+        email = students.email
+
+    message = f"Dear {name}, OTP for email verification is {OTP}"
+    from_email = settings.EMAIL_HOST_USER
+    recipient = [email]
+
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=from_email,
+        recipient_list=recipient,
+    )
--- skbackend/skbackend/urls.py
+++ skbackend/skbackend/urls.py
@@ -16,13 +16,18 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
 from django.shortcuts import HttpResponse
+from django.urls import include, path
+
 
 def entry_point(request):
     return HttpResponse()
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls'),)
+    path("admin/", admin.site.urls),
+    path(
+        "api/",
+        include("api.urls"),
+    ),
 ]
