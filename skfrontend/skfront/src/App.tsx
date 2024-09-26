a3ebcb2ba09814ccd83c5f9673a077181052499b
Kirtirajsinh08 <140191376+Kirtirajsinh08@users.noreply.github.com>
2024-09-26 16:22:25 +0530

    Some Changes

--- skfrontend/skfront/src/App.tsx
+++ skfrontend/skfront/src/App.tsx
@@ -10,15 +10,8 @@ import ContactUs from './components/ContactUs'
 import School from './components/School'
 import UserProfile from './components/UserProfile'
 import Educator from './components/Educator'
-<<<<<<< HEAD
-import Hackout from './components/Hackout'
-
-
-
-=======
 import ProtectedRoutes from './components/ProtectedRoutes'
 import { makeRegiserRequest } from './api'
->>>>>>> 5072da1dd852c2ac8939cf1413d8673b22886c7c
 function Logout() {
     localStorage.clear()
     return <Navigate to="/" />
@@ -31,26 +24,7 @@ function RegisterAndLogout() {
 
 function App() {
     return (
-<<<<<<< HEAD
-        // <BrowserRouter>
-        //     <Routes>
-        //         <Route path='/' element={<ProtectedRoutes><Home /></ProtectedRoutes>} />
-        //         <Route path='/login' element={<Login />} />
-        //         <Route path='/logout' element={<Logout />} />
-        //         <Route path='/register' element={<RegisterAndLogout />} />
-        //         <Route path='/educatorForm' element={<EduForm1 />} />
-        //         <Route path='/educator' element={<Educator />} />
-        //         <Route path='/student' element={<UserProfile />} />
-        //         <Route path='/contactus' element={<ContactUs />} />
-        //         <Route path='/school' element={<School />} />
-        //         <Route path='*' element={<NotFound />} />
-        //     </Routes>
-        //     <Footer />
-        // </BrowserRouter>
 
-        <><Hackout /></>
-        
-=======
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
@@ -67,8 +41,6 @@ function App() {
                 <Route path='*' element={<NotFound />} />
             </Routes>
         </BrowserRouter>
-
->>>>>>> 5072da1dd852c2ac8939cf1413d8673b22886c7c
     )
 }
 
